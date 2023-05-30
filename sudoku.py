f129aab293e4dccecc2cdff34d585348a4755fb0
henrydhall <henrydhall3.14@gmail.com>
2023-05-30 06:23:03 -0600

    Started messing with templates

--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '<h1>Hello there!!</h1>'
+
+@app.route('/sudoku')
+def sudoku():
+    name = None
+    return render_template('index.html')
