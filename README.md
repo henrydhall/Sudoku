53a08a576f0867d5e6d8b2ff7ef88a0350a8015c
Henry Daniel Hall <henrydhall@churchofjesuschrist.org>
2023-06-06 12:11:28 -0600

    Getting input, updated README

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Experimenting with solving and generating Sudoku puzzles with Python.
 * Just download/clone, and I have powershell scripts right now to run it. I suppose I could set up bash or command prompt scripts as well, but for now that's what we've got.
 
 ## TODO
-* Make table more readable with lines.
-* Get sudoku input to server.
+* Make table more readable with lines between groups of 9.
+* Get sudokus.
 * Solve sudoku, return to a page.
-* Make sudokus.
+* Make form prettier.
 * Update todo's
--- sudoku.py
+++ sudoku.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template
 
 from flask_bootstrap import Bootstrap
 
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess huh'
 
 bootstrap = Bootstrap(app)
+
+class SudokuForm(FlaskForm):
+    sudoku_puzzle = TextAreaField("SudokuPuzzle",validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def index():
@@ -13,8 +22,12 @@ def index():
     numbers[1] = ''
     return render_template('index.html', numbers = numbers)
 
-@app.route('/solve_helper')
+@app.route('/solve_helper',methods = ['GET','POST'])
 def solve_helper():
+    form = SudokuForm()
+    if form.validate_on_submit():
+        print(form.sudoku_puzzle.data)
+        return render_template('index.html', numbers = form.sudoku_puzzle.data )
     name = None
     numbers = [i for i in range(0,81)]
-    return render_template('solve_input.html', numbers = numbers)
+    return render_template('solve_input.html', numbers = numbers, form = form)
