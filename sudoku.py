44328886151fafca3da1ef7e8e628f7e65d61fe5
Henry Daniel Hall <henrydhall@churchofjesuschrist.org>
2023-09-19 15:27:11 -0600

    added reduced puzzle, trimmed main.css

--- sudoku.py
+++ sudoku.py
@@ -36,7 +36,12 @@ def solve_helper():
     if form.validate_on_submit():
         my_puzzle = sudoku_solver.SudokuSolver(form.sudoku_puzzle.data)
         puzzle_solution = my_puzzle.get_possibilities_for_web() # TODO: make new function to make possibilities pretty
-        return render_template('index.html', numbers=puzzle_solution)
+        reduced_puzzle = my_puzzle.get_reduced_puzzle()
+        return render_template('solved.html', numbers=puzzle_solution, reduced_puzzle=reduced_puzzle)
     name = None
     numbers = [i for i in range(0, 81)]
     return render_template('solve_input.html', numbers=numbers, form=form)
+
+@app.route('/solved/')
+def solved():
+    return render_template('solved.html',numbers=sudoku_solver.BLANK_PUZZLE)
